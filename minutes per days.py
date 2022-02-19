9b99f47fb30707e58aef49f5e049064de5439776
ULTRACODE1521 <99091660+ULTRACODE1521@users.noreply.github.com>
2022-02-19 13:16:18 -0500

    Add files via upload

---
+++ minutes per days.py
@@ -0,0 +1,9 @@
+def print_minutes_per_day(days):
+    hours = days * 24
+    minutes = hours * 60
+    print  (minutes)
+
+print_minutes_per_day(1)
+print_minutes_per_day(1)
+print_minutes_per_day(1)
+print_minutes_per_day(1)
---
+++ prediction of code.py
@@ -0,0 +1,29 @@
+import turtle
+mypen = turtle.Turtle()
+#face
+mypen.goto(0,0)
+mypen.begin_fill()
+mypen.color('yellow')
+mypen.circle(110)
+mypen.end_fill()
+
+mypen.goto(-30,100)
+mypen.begin_fill()
+mypen.color('green')
+mypen.circle(30)
+mypen.end_fill()
+mypen.begin_fill()
+mypen.color('black')
+mypen.circle(20)
+mypen.end_fill()
+
+
+mypen.goto(30,100)
+mypen.begin_fill()
+mypen.color('green')
+mypen.circle(30)
+mypen.end_fill()
+mypen.begin_fill()
+mypen.color('black')
+mypen.circle(20)
+mypen.end_fill()
