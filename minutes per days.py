d3e398255bf057864c47d358e1dfa35c680b103c
ULTRACODE1521 <99091660+ULTRACODE1521@users.noreply.github.com>
2022-04-02 13:03:32 -0400

    Delete minutes per days.py

--- minutes per days.py
+++
@@ -1,9 +0,0 @@
-def print_minutes_per_day(days):
-    hours = days * 24
-    minutes = hours * 60
-    print  (minutes)
-
-print_minutes_per_day(1)
-print_minutes_per_day(1)
-print_minutes_per_day(1)
-print_minutes_per_day(1)
