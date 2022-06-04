172b535efb75068f575a5942e695443d39ceb6d0
ULTRACODE1521 <99091660+ULTRACODE1521@users.noreply.github.com>
2022-06-04 12:35:40 -0400

    Delete Python201 code directory

--- Python201 code/Python201 code/dictionary/dictionary2.py
+++
@@ -1,8 +0,0 @@
-import json
-data = json.load(open("data.json"))
-
-def translate(word):
-    return data [word]
-
-word = input("Enter a word please: ")
-print(translate(word))
--- Python201 code/py200 circumfrence.py
+++
@@ -1,6 +0,0 @@
-rad = 12
-pi = 3.14
-
-cir = rad * pi * 2
-
-print (cir)
