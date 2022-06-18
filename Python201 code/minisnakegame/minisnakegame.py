788cc07f99ed3e4deaebc0160faba0427b9fcbf0
ULTRACODE1521 <99091660+ULTRACODE1521@users.noreply.github.com>
2022-06-18 12:41:04 -0400

    Add files via upload

---
+++ Python201 code/minisnakegame/minisnakegame.py
@@ -0,0 +1,31 @@
+import math 
+import random
+from turtle import color
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+class cube(object):
+    rows =20
+    w=500
+    def __init__(self, start, dirnx = 1, dirny = 0. color=(255,0,0)):
+        self.pos = start
+        self.dirnx=1
+        self.dirny=0
+        self.color=color
+    def move (self, dirnx, dirny):
+        self.dirnx=dirnx
+        self.dirny=dirny
+        self.pos = (self.pos[0] + self. dirnx, self.pos[1] + self.dirny)
+
+    def draw (self,surface,eyes=False):
+        dis = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
+        
+        pygame.draw.rect(surface,self.color, (i*dis+1, dis-2, dis-2))
+        if eyes:
+            centre = dis//2
+            radius = 3
+            circleMiddle= (i.dis+centre-radius, j*dis+8)
+            circleMiddle2= (i.dis+centre-radius)
